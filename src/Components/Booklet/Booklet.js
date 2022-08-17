5d2fdbe410bdf38cff21a622bd3808a32a838c8b
Mhorvilleur13 <mhorvilleur13@gmail.com>
2022-08-17 16:01:31 -0500

    added the form and the ability to make many booklets

--- src/Components/Booklet/Booklet.js
+++ src/Components/Booklet/Booklet.js
@@ -1,43 +1,44 @@
 import React from "react";
 import Carousel from "react-bootstrap/Carousel";
-import { useState } from "react";
-import { Worker } from "@react-pdf-viewer/core";
-import { Viewer } from "@react-pdf-viewer/core";
 // Import the styles
 import "@react-pdf-viewer/core/lib/styles/index.css";
 import ELFlogo from "../../Assets/images/ELP_Logo_TaglineStacked_Blue.png";
 import "../../index.css";
 
-const Booklet = ({ pdfFiles }) => {
+const Booklet = ({ allPdfFiles, booklets, currentPdfFiles }) => {
   return (
     <div className="booklet-section row">
       <div className="col-md-8 col-sm-9 col-lg-6 col-xl-5 mx-auto">
-        {pdfFiles.length >= 1 ? (
-          <Carousel interval={null}>
-            <Carousel.Item>
-              <h4 className="text-center">Cover</h4>
-              <div className="cover">
-                <img src={ELFlogo} className="cover-logo"></img>
-                <h2>Title</h2>
-                <h3>Author</h3>
-              </div>
-            </Carousel.Item>
-            {pdfFiles.map((pdfFile, index) => {
+        {booklets.length >= 1
+          ? booklets.map((booklet) => {
               return (
-                <Carousel.Item>
-                  <h4 className="text-center">
-                    Page {index + 1} / {pdfFiles.length}
-                  </h4>
-                  <embed src={pdfFile} className="w-100 page" />
-                </Carousel.Item>
+                <Carousel interval={null}>
+                  <Carousel.Item>
+                    <h4 className="text-center">Cover</h4>
+                    <div className="cover">
+                      <img src={ELFlogo} className="cover-logo"></img>
+                      <h1>{booklet.title}</h1>
+                      <br></br>
+                      <h3>By</h3>
+                      <h3>{booklet.teacher}</h3>
+                    </div>
+                  </Carousel.Item>
+                  {booklet.files.map((pdfFile, index) => {
+                    return (
+                      <Carousel.Item>
+                        <h4 className="text-center">
+                          Page {index + 1} / {booklet.files.length}
+                        </h4>
+                        <embed src={pdfFile} className="w-100 page" />
+                      </Carousel.Item>
+                    );
+                  })}
+                </Carousel>
               );
-            })}
-          </Carousel>
-        ) : (
-          pdfFiles.length < 1 && (
-            <h3 className="text-center">No File Selected</h3>
-          )
-        )}
+            })
+          : booklets.length < 1 && (
+              <h3 className="text-center">No File Selected</h3>
+            )}
       </div>
     </div>
   );
--- src/atom.js
+++ src/atom.js
@@ -1,11 +1,20 @@
-import {atom} from 'recoil';
+import { atom } from "recoil";
+
+export const bookletsState = atom({
+  key: "booklets",
+  default: [],
+});
+export const allPdfFilesState = atom({
+  key: "all-pdf-files",
+  default: [],
+});
 
 export const pdfFilesState = atom({
-    key: "pdf-files", 
-    default: []
-})
+  key: "pdf-files",
+  default: [],
+});
 
 export const currentPageState = atom({
-    key: "current-page", 
-    default: []
-})
+  key: "current-page",
+  default: [],
+});
