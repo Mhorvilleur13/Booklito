d0a3af00f2c3a572425621b23ad2cb47396f744b
Mhorvilleur13 <mhorvilleur13@gmail.com>
2022-10-17 10:33:56 -0500

    only returns user's booklets

--- src/Auth.js
+++ src/Auth.js
@@ -1,13 +1,22 @@
 import React, { useEffect, useState } from "react";
 import { db, auth, app } from "./firebase-config";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { useRecoilState } from "recoil";
+import { userEmailState as userEmailAtom } from "./atom";
 
 export const AuthContext = React.createContext();
 
 export const AuthProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(null);
+  const [userEmail, setUserEmail] = useRecoilState(userEmailAtom);
+  // useEffect(() => {
+  //   onAuthStateChanged(auth, setCurrentUser);
+  // }, []);
   useEffect(() => {
-    onAuthStateChanged(auth, setCurrentUser);
+    onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user);
+      setUserEmail(user?.email);
+    });
   }, []);
   return (
     <AuthContext.Provider value={{ currentUser }}>
